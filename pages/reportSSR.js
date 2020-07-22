f2eaeb53b41d3d45c34da27af8bb8243323ca654
Andrew Watkins <andrew@groat.nz>
2020-07-23 11:42:43 +1200

    setup vercel

--- pages/reportSSR.js
+++ pages/reportSSR.js
@@ -3,38 +3,15 @@ import Layout from '../components/layout'
 import { useFetchUser } from '../lib/user'
 import config from '../lib/config'
 import EventsTable from '../components/EventsTable'
-import { useState } from 'react'
 
-const fetchEvents = async () => {
-  const res = await fetch( `${config.HOST}/api/events` )
-  return res.json()
-}
-
-function Report ({ serverEvents }) {
+function Report ({ events }) {
   // set required to true to force the page to require login.
   const { user, loading } = useFetchUser({ required: true })
-  const [ events, setEvents ] = useState([])
-  // fetch data
-  useEffect(() => {
-    const evts = await fetchEvents()
-    setEvents(evts)
-  }, [])
 
   return (
     <Layout user={user} loading={loading}>
-      <h1>Results</h1>
-
-      {loading && <p>Loading login info...</p>}
-      {!loading && !user && (
-        <>
-          <p>
-            To view the results <a href='/api/login'>Login</a>
-          </p>
-        </>
-      )}
-      {user && (
-        <EventsTable user={user} events={events} />
-      )}
+      <h1>Results {events.length}</h1>
+      <EventsTable events={events} />
     </Layout>
   )
 }
@@ -46,7 +23,7 @@ export async function getServerSideProps () {
   const events = await res.json()
 
   // Pass data to the page via props
-  return { props: { serverEvents } }
+  return { props: { events } }
 }
 
 export default Report
