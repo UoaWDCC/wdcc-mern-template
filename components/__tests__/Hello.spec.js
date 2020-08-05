c5f936009b8daa2e6c569138ece0425cc9200a33
Andrew Watkins <andrew@groat.nz>
2020-08-05 18:14:17 +1200

    add test for ProfileCard

--- components/__tests__/Hello.spec.js
+++ components/__tests__/Hello.spec.js
@@ -1,23 +1,22 @@
 import * as React from 'react'
-import {mount, shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import { Hello, Greet } from '../Hello'
 
 describe('Hello Components', () => {
-    it('should render without throwing an error', function () {
-      const wrap = mount(<Greet />)
-      expect(wrap.find('p').text()).toBe('Hello, *Your name here*!')
-      expect(wrap.find('p').text()).toMatch(/Hello/)
-    })
+  it('should render without throwing an error', function () {
+    const wrap = mount(<Greet />)
+    expect(wrap.find('p').text()).toBe('Hello, *Your name here*!')
+    expect(wrap.find('p').text()).toMatch(/Hello/)
+  })
 
-    it('should render the persons name', function () {
-      const wrap = mount(<Hello name='Testy' />)
-      expect(wrap.find('p').text()).toBe('Hello, Testy')
-    })
+  it('should render the persons name', function () {
+    const wrap = mount(<Hello name='Testy' />)
+    expect(wrap.find('p').text()).toBe('Hello, Testy')
+  })
 
-    it('should shallow the persons name', function () {
-      const name = 'Testy McTestface'
-      const wrap = shallow(<Hello name={name} />)
-      expect(wrap.find('p').text()).toMatch(`Hello, ${name}`)
-    })
-
+  it('should shallow the persons name', function () {
+    const name = 'Testy McTestface'
+    const wrap = shallow(<Hello name={name} />)
+    expect(wrap.find('p').text()).toMatch(`Hello, ${name}`)
+  })
 })
