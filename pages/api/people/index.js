2b816904883d92a7f1a6d7cbc89f04b4664a8905
Andrew Watkins <andrew@groat.nz>
2020-08-08 14:07:35 +1200

    tidy up

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -1,18 +1,18 @@
 import nextConnect from 'next-connect'
 import fetch from 'node-fetch'
 
+/* Demonstrates fetching data from a third party API call
+*/
+
 const handler = nextConnect()
 const peopleService = 'https://randomuser.me'
 
 handler.get(async (req, res) => {
-  // find returns a cursor which we need to iterate through to get the results.
-  // use next or toArray
-  console.log('people.get', req.query)
-  const count = req.query.count || 1
+  const count = req.query.count || 1 // always backstop query parameters
   const url = `${peopleService}/api/?results=${count}`
   const doc = await fetch(url)
   const json = await doc.json(doc)
-  console.log(json.results)
+  // console.log(json.results)
   res.json(json.results)
 })
 
